df0e15db2901e905c2be6ffb948266d8c619ac60
SteveBeaupre <stevebeaupre1981@gmail.com>
2018-04-25 02:33:38 -0400

    Almost done
    Just need to fix the wrap around bug in CountNeighborgs

--- Life.asm
+++ Life.asm
@@ -1,11 +1,12 @@
 main:
 
 	fim r14 $77
-
 	jms ReadInput
 
 	fim r12 $00
-	jms CountNeighborgs
+	fim r14 $77
+	jms CalcNextGen
+
 	jms PrintMemory
 	
 	jun end_prog
@@ -13,7 +14,7 @@ main:
 ReadInput:
 
 	jms ReadCharacter
-	jms PrintCharacter
+	;jms PrintCharacter
 	;jms PrintRegisters
 	
 	ld  r3
@@ -32,7 +33,7 @@ ReadInput:
 	
 	jms ReadCharacter
 	;fim r2 $20
-	jms PrintCharacter
+	;jms PrintCharacter
 	
 	ldm 0
 	xch r13
@@ -83,13 +84,18 @@ CountNeighborgs:
 	
 	src r0
 	rdm
-	clc
-	add r4
+	jcn az NotAlive
+	ld  r4
+	iac
 	xch r4
-	src r12
-	wrm	
+	NotAlive:
+	;clc
+	;add r4
+	;xch r4
+	;src r12
+	;wrm	
 	
-	jms PrintRegisters
+	;jms PrintRegisters
 
 	SkipCell:
 	clc
@@ -119,6 +125,49 @@ CountNeighborgs:
 	
 	bbl 0
 	
+CalcNextGen:
+
+	jms CountNeighborgs
+	
+	jms PrintRegisters
+
+	clc
+	ld  r13
+	xch r1
+	ldm 8
+	add r12
+	xch r0
+	src r0
+	ld  r4
+	wrm	
+	
+	clc
+	ld  r13
+	iac
+	xch r13
+	clc
+	ld  r15
+	dac
+	xch r15
+	jcn c1 CalcNextGen
+	
+	ldm 0
+	xch r13
+	ldm 7
+	xch r15
+	
+	clc
+	ld  r12
+	iac
+	xch r12
+	clc
+	ld  r14
+	dac
+	xch r14
+	jcn c1 CalcNextGen
+
+	bbl 0
+	
 ReadCharacter:
 	jms $3f0
 	bbl 0
@@ -134,10 +183,45 @@ PrintNewLine:
 	jms $3e0
 	bbl 0
 	
+;PrintMemory:
+;	jms PrintNewLine
+;	jms $3fd
+;	jms PrintNewLine
+;	bbl 0
+	
 PrintMemory:
+	fim r0 $00
+	;fim r2 $30
+
+	NextCell:
+	
+	;jms PrintRegisters
+	
+	src r0
+	rdm
+	xch r3
+	ldm 3
+	xch r2
+	jms PrintCharacter
+
+	clc
+	ld  r1
+	iac
+	xch r1
+	jcn c0 NextCell
+
+	ldm 0
+	xch r1
 	jms PrintNewLine
-	jms $3fd
+	
+	clc
+	ld  r0
+	iac
+	xch r0
+	jcn c0 NextCell
+
 	jms PrintNewLine
+
 	bbl 0
 	
 PrintRegisters:
