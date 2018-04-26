c9389094f2079aa1e0270405ab9180f9714a0f3b
SteveBeaupre <stevebeaupre1981@gmail.com>
2018-04-26 14:57:56 -0400

    Rewrote the whole thing...
    Just need to calculate the next gen now

--- Life.asm
+++ Life.asm
@@ -1,246 +1,225 @@
+jun main
+
+Offset: db $FF $F0 $F1 $0F $01 $1F $10 $11
+
 main:
 
-	fim r14 $77
 	jms ReadInput
-
-	;jun TEST
-	fim r12 $00
-	fim r14 $77
+	
+	;fim r0 $00
+	;jms CountNeighbors
+	
 	jms CalcNextGen
-	;jms PrintMemory
-	;jms PrintRegisters
+	
+	jms PrintMemory
+
 	jun end_prog
 	
-	TEST:
-	fim r12 $33
-	fim r14 $77
-	jms CountNeighborgs
-	;jms PrintRegisters
-
-	jun end_prog
 	
 ReadInput:
-
-	jms ReadCharacter
-	;jms PrintCharacter
-	;jms PrintRegisters
+	fim r0  $00
+	fim r14 $88
+	jun NextCol2
 	
-	ld  r3
-	src r12
-	wrm	
+	NextRow2:
+		ldm 8
+		xch r15
+		NextCol2:
+			
+			jms ReadChar
+			
+			ld  r3
+			src r0
+			wrm
+			
+			clc
+			ld  r1
+			iac
+			xch r1
+			clc
+			
+		isz r15 NextCol2
 		
-	clc
-	ld  r13
-	iac
-	xch r13
-	clc
-	ld  r15
-	dac
-	xch r15
-	jcn c1 ReadInput
-	
-	jms ReadCharacter
-	;fim r2 $20
-	;jms PrintCharacter
-	
-	ldm 0
-	xch r13
-	ldm 7
-	xch r15
-	
-	clc
-	ld  r12
-	iac
-	xch r12
-	clc
-	ld  r14
-	dac
-	xch r14
-	jcn c1 ReadInput
-	
-	;jms PrintMemory
-	
+		ldm 0
+		xch r1
+		
+		clc
+		ld  r0
+		iac
+		xch r0
+		clc
+		
+		jms ReadChar
+		
+	isz r14 NextRow2
+
 	bbl 0
 	
-CountNeighborgs:
+CountNeighbors:
+	fim r8 $80
 
-	fim r4 $04
-	fim r6 $22
-	fim r8 $FF
-	
-	;jms PrintRegisters
-	NextOne:
-	
-	ld  r13
-	xch r11
-	ld  r12
-	xch r10
-
-
-	clc
-	ld  r9
-	add r13
-	xch r1
-	clc
-	ld  r8
-	add r12
-	xch r0
-
-	ld  r5
-	dac
-	xch r5
-	jcn az SkipCell
-	
-	clc
-	ld  r1
-	ral
-	jcn c1 SkipCell
-	clc
 	ld  r0
-	ral
-	jcn c1 SkipCell
+	xch r12
+	ld  r1
+	xch r13
 	
-	src r0
-	rdm
-	jcn az NotAlive
-	ld  r4
-	iac
-	xch r4
-	NotAlive:
+	fim r0 Offset
 	
-	;jms PrintRegisters
-
-	SkipCell:
+	NextNeighbor:
+	
+		fin r10
+	
+		ld  r12
+		xch r14
+		ld  r13
+		xch r15
+	
+		clc
+		ld  r15
+		add r11
+		xch r15
+		clc
+		ld  r14
+		add r10
+		xch r14
+	
+		ld r15
+		ral
+		jcn c1 InvalidCell
+		ld r14
+		ral
+		jcn c1 InvalidCell
+		
+		clc
+		src r14
+		rdm
+		
+		clc
+		add r9
+		xch r9
+		
+		InvalidCell:
+		clc
+		ld  r1
+		iac
+		xch r1
+		jcn c0 NextOffset
+		
+		clc
+		ld  r0
+		iac
+		xch r0
+		NextOffset:
+		
+	isz r8 NextNeighbor
+	
+	ld  r12
+	xch r0
+	ld  r13
+	xch r1
+	
 	clc
+	ldm 8
+	add r13
+	xch r13
+	
 	ld  r9
-	iac
-	xch r9
-	clc
-	ld  r7
-	dac
-	xch r7
-	jcn c1 NextOne
-	
-	ldm 2
-	xch r7
-	ldm 15
-	xch r9
-	
-	clc
-	ld  r8
-	iac
-	xch r8
-	clc
-	ld  r6
-	dac
-	xch r6
-	jcn c1 NextOne
+	src r12
+	wrm
 	
 	bbl 0
 	
 CalcNextGen:
-
-	jms CountNeighborgs
-	
-	jms PrintRegisters
-
-	clc
-	ld  r13
-	xch r1
-	ldm 8
-	add r12
-	xch r0
-	src r0
-	ld  r4
-	wrm	
-	
-	clc
-	ld  r13
-	iac
-	xch r13
-	clc
-	ld  r15
-	dac
-	xch r15
-	jcn c1 CalcNextGen
-	
-	ldm 0
-	xch r13
-	ldm 7
-	xch r15
-	
-	clc
-	ld  r12
-	iac
-	xch r12
-	clc
-	ld  r14
-	dac
-	xch r14
-	jcn c1 CalcNextGen
-
-	bbl 0
-	
-ReadCharacter:
-	jms $3f0
-	bbl 0
-	
-PrintCharacter:
-	jms $3e0
-	bbl 0
-	
-PrintNewLine:
-	fim r2 $0D
-	jms $3e0
-	fim r2 $0A
-	jms $3e0
-	bbl 0
-	
-;PrintMemory:
-;	jms PrintNewLine
-;	jms $3fd
-;	jms PrintNewLine
-;	bbl 0
-	
-PrintMemory:
 	fim r0 $00
-	;fim r2 $30
-
-	NextCell:
+	fim r4 $88
 	
-	;jms PrintRegisters
+	NextRow3:
+	NextCol3:
 	
-	src r0
-	rdm
-	xch r3
-	ldm 3
-	xch r2
-	jms PrintCharacter
-
+	jms CountNeighbors
+	;jms PrintRegs
+	
 	clc
 	ld  r1
 	iac
 	xch r1
-	jcn c0 NextCell
-
+	isz r5 NextCol3
+	
 	ldm 0
 	xch r1
-	jms PrintNewLine
+	ldm 8
+	xch r5
 	
 	clc
 	ld  r0
 	iac
 	xch r0
-	jcn c0 NextCell
+	isz r4 NextRow3
 
-	jms PrintNewLine
 
 	bbl 0
 	
-PrintRegisters:
+ReadChar:
+    jms $3f0
+	bbl 0
+	
+PrintChar:
+    jms $3e0
+	bbl 0
+	
+PrintSpace:
+	fim r2 $20
+    jms PrintChar
+	bbl 0
+	
+PrintNewLine:
+	;fim r2 $0D
+	;jms PrintChar
+	fim r2 $0A
+	jms PrintChar
+	bbl 0
+	
+PrintHex:
+	clc
+	fim r2 $30
+	xch r3
+	ld  r3
+	daa
+	jcn c1 Alpha
+	jun NotAlpha
+	Alpha:
+		clc
+		ldm 7
+		add r3
+		xch r3
+		clc
+		ld  r2
+		iac
+		xch r2
+	NotAlpha:
+		clc
+	jms PrintChar
+	
+	bbl 0
+	
+PrintMemory:
+	fim r0 $00
+	jun NextCol
+	
+	NextRow:
+		jms PrintNewLine
+		NextCol:
+			src r0
+			rdm
+			jms PrintHex
+		isz r1 NextCol
+	isz r0 NextRow
+		
+	bbl 0
+
+PrintRegs:
 	jms $3ff
 	bbl 0
-	
+
 end_prog:
-	jms PrintNewLine
-	;jms PrintRegisters
+	nop
